d05f3fe3fa7611e095884b048e6d661774348ef1
Calliel Freitas <49662240+calfreitas@users.noreply.github.com>
2023-08-24 15:56:39 -0300

    Update Project_PDFmerger.py

--- Project_PDFmerger.py
+++ Project_PDFmerger.py
@@ -3,12 +3,18 @@ import os
 
 merger = PyPDF2.PdfMerger()
 
-archiv_list = os.listdir("archives")
+# (1). "Rename_Folder" is where your ".pdf" archives need to be.
+# (2). This part will sort your archives in sequence (numeric)
+
+archiv_list = os.listdir("Rename_folder")
 archiv_list.sort()
 print(archiv_list)
 
+# (1).
+
 for archive in archiv_list:
     if '.pdf' in archive:
-        merger.append(f"archives/{archive}")
-
-merger.write("PDF Final.pdf")
+        merger.append(f"Rename_folder/{archive}")
+        
+# (3). You can RENAME your "file.pdf" here.
+merger.write("Rename_or_not_Final_PDF.pdf")
